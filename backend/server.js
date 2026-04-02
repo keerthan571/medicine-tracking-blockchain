34577d951971da6d2a6df388cad0169de5cca6ee
mahesha8970 <mahesha05ramesh@gmail.com>
2026-04-03 00:03:14 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,19 @@
+const express = require("express");
+const cors = require("cors");
 
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// test route
+app.get("/", (req, res) => {
+  res.send("Backend working 🚀");
+});
+
+// import routes
+const medicineRoutes = require("./routes/medicine");
+app.use("/api", medicineRoutes);
+
+app.listen(5000, () => {
+  console.log("Server running on http://localhost:5000");
+});
